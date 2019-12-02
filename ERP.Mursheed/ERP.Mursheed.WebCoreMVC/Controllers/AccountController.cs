bfdb32f91abd73d327335422ce976d7a0c0cad02
parvizrovshanaliyev <parvizra@code.edu.az>
2019-12-02 16:53:34 +0400

    .....

--- ERP.Mursheed/ERP.Mursheed.WebCoreMVC/Controllers/AccountController.cs
+++ ERP.Mursheed/ERP.Mursheed.WebCoreMVC/Controllers/AccountController.cs
@@ -1,26 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using ERP.Mursheed.Entities.Shared;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ERP.Mursheed.WebCoreMVC.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
+        [Route("Account/{login}")]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Account/{login}")]
+        public IActionResult Login(Person person) // todo
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("{register}")]
         public IActionResult Register()
         {
             return View();
         }
-        public IActionResult DriverRegister()
+
+        #region Register Driver
+        [HttpGet]
+        [Route("register/{driver}")]
+        public IActionResult Driver()
         {
             return View();
         }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("register/{guide}")]
+        public IActionResult Driver(Person person) // todo viewmodel
+        {
+            return View();
+        }
+        #endregion
+
+        #region Register Guide
+        [HttpGet]
+        [Route("register/{guide}")]
+        public IActionResult Guide()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("register/{driver}")]
+        public IActionResult Guide(Person person) // todo viewmodel
+        {
+            return View();
+        }
+        #endregion
+
+        #region Register Tourist
+        [HttpGet]
+        [Route("register/{tourist}")]
+        public IActionResult Tourist()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("register/{tourist}")]
+        public IActionResult Tourist(Person person) // todo viewmodel
+        {
+            return View();
+        }
+        #endregion
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Logout()
         {
             return View();
